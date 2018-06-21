422894a5302c135841ce83c74b2b26de033ee7d6
FelipeSepulvedaRozas <felipe.s.rozas@gmail.com>
2018-06-21 12:10:22 -0400

    solved item 1-2

--- mantenedor.rb
+++ mantenedor.rb
@@ -1,23 +1,60 @@
 
-
-def menu
-  puts 'Ingrese 1) crear archivo de alumno'
+def menuprincipal
+  puts 'Ingrese 1) crear archivo de Promedios'
   puts 'Ingrese 2) asistencias '
   puts 'Ingrese 3) aprobacion'
   puts 'Ingrese 4) para salir'
 end
 
 
+def txtnotas
+  file = File.open('alumnos.csv', 'r')
+  data = file.readlines
+  file.close
+  data
+end
+
+def txtpromedio
+  file = File.open('promedio.csv', 'r')
+  data =cd  file.readlines
+  file.close
+  data
+end
+
+
+
 loop do |option|
-  menu
+  menuprincipal
   option = gets.chomp
   case option.to_i
+    
   when 1
-    puts 'alumonos'
+    
+  
+    lines = []
+    File.open('alumnos.csv','r'){|file| lines = file.readlines }
+    lines.each do |value|
+        sum = 0
+    alumno = value.split(', ').map(&:chomp)
+    alumno.each do |i|
+      sum += i.to_i if i.to_i.is_a? Integer
+    end
+    puts "#{alumno[0]}:#{sum.to_f/(alumno.length - 1)}"
+  end
+  
+
   when 2
-    puts 'asistencia'
+    puts 'Cantidad de Ausencias por alumno'
+    lines =[]
+    File.open('alumnos.csv','r'){|file| lines = file.readlines }
+    lines.each do |value|
+      countA = 0
+      asistencia = value.split(', ').map(&:chomp)
+      countA += asistencia.count('A')
+      puts "#{asistencia[0]}:#{countA}"
+    end
   when 3
-  puts 'Aprobados'
+    puts 'aprobados'
   when 4
     break
   else
@@ -25,3 +62,7 @@ loop do |option|
   end
   break if option == 4
 end
+
+
+
+
